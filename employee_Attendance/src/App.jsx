3477b80476f67cfd4659ff50a84c1c90f9c7a87a
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-09-10 12:27:42 +0530

    added v0 and shadcn

--- employee_Attendance/src/App.jsx
+++ employee_Attendance/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EmployeeList from './components/EmployeeList';
 import EmployeeDetail from './components/EmployeeDetail';
 import CreateEmployee from './components/CreateEmployee';
+import Dashboard from './dashboard';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<EmployeeList />} />
                 <Route path="/employees/:id" element={<EmployeeDetail />} />
                 <Route path="/create-employee" element={<CreateEmployee />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
     );
